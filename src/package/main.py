409a1fb1eb325026c083aad69618e218a213a59a
purmirl <gmlrnjs1994@gmail.com>
2021-08-07 23:54:14 +0900

    20210807 : main is modified.

--- src/package/main.py
+++ src/package/main.py
@@ -1,5 +1,23 @@
 """
- Copyright 2020 PeTrA. All rights reserved.
+ Copyright 2021~ PeTrA. All rights reserved.
  . Python Project Structure Repository;
+
+ Lightning Arrow Project (UDP Launcher) by PeTrA. 2021~
+ LightningArrow 1.0
+ Language : Python3.8.2 on pycharm IDE
+ Library : Scapy2.4.3
+ ------
+ @ main.py
+    * LightningArrow/src/package/main.py
+    * main function code file
 """
-# PythonProjectStructure/src/main.py;
+
+""" @main function
+"""
+def main():
+    return
+
+""" @call main
+"""
+if __name__ == "__main__":
+    main()
