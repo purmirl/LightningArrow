23722da18bfa11fda7ee1417cd73be575866b77a
purmirl <gmlrnjs1994@gmail.com>
2021-08-12 22:15:16 +0900

    20210812 : cui is added.

---
+++ src/package/cui.py
@@ -0,0 +1,18 @@
+"""
+ Copyright 2021~ PeTrA. All rights reserved.
+ . Python Project Structure Repository;
+
+ Lightning Arrow Project (UDP Launcher) by PeTrA. 2021~
+ LightningArrow 1.0
+ Language : Python3.8.2 on pycharm IDE
+ Library : Scapy2.4.3
+ ------
+ @ cui.py
+    * LightningArrow/src/package/cui.py
+    * console user interface code file
+"""
+
+class Cui:
+    def __init__(self):
+        return
+
